0b1aea864fa0369206caff753e898732c7e3a683
HuasLY <huas.ly.42@gmail.com>
2015-02-22 01:09:30 +0100

    modal transition slide

--- likcode.js
+++ likcode.js
@@ -1,28 +1,21 @@
 var likcode = {
     init: function() {
         this.appendCss();
-        this.appendHtml();
-        this.eventHandler();
+        this.prependHtml();
         this.changeNavBarColor();
     },
 
     appendCss: function() {
-        $('head').append('<style>.modalDialog { position: fixed;font-family: Arial, Helvetica, sans-serif;top: 0;right: 0;bottom: 0;left: 0;background: rgba(0, 0, 0, 0.8);z-index: 1000;-webkit-transition: opacity 400ms ease-in;-moz-transition: opacity 400ms ease-in;transition: opacity 400ms ease-in;}' +
-        '.modalDialog:target {opacity:1;pointer-events: auto;}' +
-        '#introModalContent {width: 400px;min-height:500px;position: fixed;top: 20%;left: 50%;margin-left: -200px;padding: 5px 20px 13px 20px;border-radius: 10px;background: #fff;background: -moz-linear-gradient(#fff, #999);background: -webkit-linear-gradient(#fff, #999);background: -o-linear-gradient(#fff, #999);z-index:4242}</style>');
+        $('head').append('<link href="' + chrome.extension.getURL("introModal.css") + '" rel="stylesheet" type="text/css">');
     },
 
-    appendHtml: function() {
-        $('div:first').prepend(
-            '<div id="introModal">' +
-                '<div id="introModalBackground" class="modalDialog"></div>' +
-                '<div id="introModalContent">' +
-                    '<h2>Modal</h2>' +
-                    '<p>Test de modal</p>' +
-                    '<div style="width: 214px;background-image: url("chrome.extension.getURL("/favicons/example.png");")">' +
-                    '<button id="introButton"> OK </button>' +
-                '</div>' +
-            '</div>');
+    prependHtml: function() {
+        var that = this;
+        $('div:first').prepend('<div id="likcodeWrapper"></div>');
+        $('#likcodeWrapper').load(chrome.extension.getURL("introModal.html"), function() {
+            that.eventHandler();
+            $('#introModalSlide').css("background", "url(" + chrome.extension.getURL("42c.png") + ")");
+        });
     },
 
     changeNavBarColor: function() {
@@ -35,7 +28,7 @@ var likcode = {
 
     eventHandler: function() {
         var that = this;
-        $('#introModalBackground, #introButton').click(function() {
+        $('#introModalBackground').click(function() {
             that.clickIntro();
         });
     }
@@ -43,4 +36,6 @@ var likcode = {
 
 $(document).ready(function() {
     likcode.init();
+    var imgURL = chrome.extension.getURL("42c.png");
+    //console.log(imgURL);
 });
